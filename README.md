8f3f2c79f0e41ffff3e79f96160f0baff7852643
Monique Messié <monique@mbari.org>
2017-08-10 16:33:51 -0700

    missed this one

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # climate_indices_visu
-Visualization of common climate indices.
+Visualization of global climate indices generated from an EOF on 100-yr SST time series with common climate indices.
+See http://www.mbari.org/science/upper-ocean-systems/biological-oceanography/global-modes-of-sea-surface-temperature/ for the method and data.
 
 This repo contains 2 independant tools: 
-- a R notebook hosted at 
+- a R notebook hosted at https://messiem.github.io/climate_indices_visu/ 
 - a Shiny app hosted at https://messiem.shinyapps.io/climate_indices/ 
--- climate_indices/app.R
+++ climate_indices/app.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(shiny)
 
+
 # Read data
 indices <- read_csv(file="http://www3.mbari.org/science/upper-ocean-systems/biological-oceanography/GlobalModes/climate_indices.csv",comment="%")
 nb_time <- length(indices$year)
@@ -8,47 +9,49 @@ indices$time <- as.Date(paste(indices$year,indices$month,rep(15,nb_time)),format
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Climate indices visualization"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        selectInput("indexname",label="Parameter",choices=c("MEI","PDO","NPGO","EMI","Nino3","Nino4","AMO","NAO","ATL3")),
-         sliderInput("bins",
-                     "Years:",
-                     min = min(indices$year),
-                     max = max(indices$year),
-                     value = max(indices$year))
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  
+  # Application title
+  titlePanel("Climate indices visualization"),
+  
+  # Sidebar with indices selection
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("indexname1",label="Index 1 (shaded)",choices=c("M1","M2","M3","M4","M5","M6")),
+      selectInput("indexname2",label="Index 2 (line)",choices=c("MEI","PDO","NPGO","EMI","Nino3","Nino4","AMO","NAO","ATL3"))
+    ),
+    
+    # Show a plot comparing climate indices
+    mainPanel(
+      plotOutput("distPlot")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  output$distPlot <- renderPlot({
 
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      # x    <- indices$year
-      # bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      # draw the histogram with the specified number of bins
-      # hist(x, breaks = bins, col = 'darkgray', border = 'white')
-     iok1 <- !is.na(indices[,input$indexname])
-     x <- indices$time
-     y0 <- rep(0,nb_time)
-     plot(indices$time,t(indices[,input$indexname]),type="l",xlab="Time",ylab=input$indexname)
-     polygon(c(x[iok1],rev(x[iok1])),c(y0[iok1],rev(t(indices[iok1,input$indexname]))),col="skyblue")
-     
-
-   })
+    iok1 <- !is.na(Mset[,input$indexname1])
+    x <- Mset$time
+    y0 <- rep(0,length(Mset$YEAR))
+    
+    # plot 1: shaded Mset (indexname1)
+    par(mar = c(5,5,2,5))
+    plot(x,t(Mset[,input$indexname1]),type="l",xlab="Time",ylab=input$indexname1)
+    polygon(c(x[iok1],rev(x[iok1])),c(y0[iok1],rev(t(Mset[iok1,input$indexname1]))),col="skyblue")
+    
+    # plot 2: line climate index (indexname2)
+    par(new=T)
+    plot(indices$time,t(indices[,input$indexname2]),type="l",axes=F,xlab=NA, ylab=NA,col="red")
+    axis(side = 4,col="red")
+    mtext(side = 4, line = 3, input$indexname2,col="red")
+    
+    #legend
+    legend("topleft",legend=c(input$indexname1,input$indexname2),lty=c(1,1),col=c("black", "red"))
+    
+  })
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
-
