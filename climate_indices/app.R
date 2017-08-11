5fc90d3cac4cd73052f2fc1620d328989136f5f7
Monique Messié <monique@mbari.org>
2017-08-11 14:49:13 -0700

    Update using ggplot & plotly. Interactive plot with anomalies shading and displaying correlation.

--- climate_indices/app.R
+++ climate_indices/app.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(plotly)
 library(shiny)
 
 
@@ -20,12 +21,13 @@ ui <- fluidPage(
   sidebarLayout(
     sidebarPanel(
       selectInput("indexname1",label="Index 1 (shaded)",choices=c("M1","M2","M3","M4","M5","M6")),
-      selectInput("indexname2",label="Index 2 (line)",choices=c("MEI","PDO","NPGO","EMI","Nino3","Nino4","AMO","NAO","ATL3"))
+      selectInput("indexname2",label="Index 2 (line)",choices=c("MEI","PDO","NPGO","EMI","Nino3","Nino4","AMO","NAO","ATL3")),
+      width=2
     ),
     
     # Show a plot comparing climate indices
     mainPanel(
-      plotOutput("distPlot")
+      plotlyOutput("distPlot")
     )
   )
 )
@@ -33,26 +35,44 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output) {
   
-  output$distPlot <- renderPlot({
+  output$distPlot <- renderPlotly({
 
-    iok1 <- !is.na(Mset[,input$indexname1])
-    x <- Mset$time
-    y0 <- rep(0,length(Mset$YEAR))
+    # max & min will be used to scale the 2nd plot
+    max1 <- max(Mset[,input$indexname1],na.rm=TRUE)
+    max2 <- max(indices[,input$indexname2],na.rm=TRUE)
     
-    # plot 1: shaded Mset (indexname1)
-    par(mar = c(5,5,2,5))
-    plot(x,t(Mset[,input$indexname1]),type="l",xlab="Time",ylab=input$indexname1)
-    polygon(c(x[iok1],rev(x[iok1])),c(y0[iok1],rev(t(Mset[iok1,input$indexname1]))),col="skyblue")
+    # Mset$varpos & $varneg will be used to display anomalies
+    Mset[,"varpos"] <- Mset[,input$indexname1]
+    Mset[,"varneg"] <- Mset[,input$indexname1]
+    Mset$varpos[which(Mset$varpos > 0)] <- rep(0, length(which(Mset$varpos > 0)))
+    Mset$varneg[which(Mset$varneg < 0)] <- rep(0, length(which(Mset$varneg < 0)))
     
-    # plot 2: line climate index (indexname2)
-    par(new=T)
-    plot(indices$time,t(indices[,input$indexname2]),type="l",axes=F,xlab=NA, ylab=NA,col="red")
-    axis(side = 4,col="red")
-    mtext(side = 4, line = 3, input$indexname2,col="red")
+    # compute correlation that will be displayed in the legend
+    commonxM <- Mset$time %in% indices$time
+    commonxI <- indices$time %in% Mset$time
+    subsetM <- Mset[commonxM,input$indexname1]
+    subsetI <- indices[commonxI,input$indexname2]
+    iok1 <- !is.na(subsetM) & !is.na(subsetI)
+    r=cor(subsetM[iok1,],subsetI[iok1,])
+    legname <- paste(input$indexname2," (r = ",toString(floor(r*100)/100),")",sep="")
     
-    #legend
-    legend("topleft",legend=c(input$indexname1,input$indexname2),lty=c(1,1),col=c("black", "red"))
+    # define plot
+    p <- ggplot() +
+      geom_area(data = Mset, aes(x=time, y=varpos), fill="blue") +
+      geom_area(data = Mset, aes(x=time, y=varneg), fill="red") +
+      geom_line(data = indices, aes(x=time, y=eval(as.name(input$indexname2))/(max2/max1), color=legname),size=0.5) +
+      scale_y_continuous(sec.axis = sec_axis(trans = ~.*(max2/max1),name = input$indexname2)) +
+      scale_x_date(limits = c(min(Mset$time),max(Mset$time)),expand=c(0,0)) +
+      labs(x  ="Time", y = input$indexname1) +
+      theme(legend.key.width=unit(3,"line"),legend.background = element_rect(fill="transparent"),legend.text=element_text(size=11)) +
+      scale_colour_manual("", 
+                          breaks = c(legname),
+                          values = c("black"))
     
+    # display plot & fix legend location issue (ggplotly would remove the legend position)
+    p2 <- plotly_build(p)   
+    style(p2) %>% layout( legend = list(x = 0.01, y = 1) )  
+      
   })
 }
 
