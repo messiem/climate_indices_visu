ed58f263ad7b48964d4c5fe40a258c923ea6f559
messiem <monique@mbari.org>
2023-08-08 13:45:59 -0700

    Added option for None (index) and fixed data origin link

--- climate_indices/app.R
+++ climate_indices/app.R
@@ -1,80 +1,104 @@
+library(shiny)
 library(tidyverse)
 library(plotly)
-library(shiny)
-
 
 # Read Mset
-Mset <- read_delim(file="http://www3.mbari.org/science/upper-ocean-systems/biological-oceanography/GlobalModes/Mset.txt",comment="%",delim="\t")
-Mset$time <- as.Date(paste(Mset$YEAR,Mset$MONTH,rep(15,length(Mset$YEAR))),format="%Y %m %d")
+Mset <- read_delim(file="https://data.mbari.org/products/satellite-derived/global-modes/Mset.txt", comment = "%", delim = "\t")
+Mset$time <- as.Date(paste(Mset$YEAR, Mset$MONTH, rep(15, length(Mset$YEAR))), format = "%Y %m %d")
 
 # Read other climate indices
-indices <- read_csv(file="http://www3.mbari.org/science/upper-ocean-systems/biological-oceanography/GlobalModes/climate_indices.csv",comment="%")
-indices$time <- as.Date(paste(indices$year,indices$month,rep(15,length(indices$year))),format="%Y %m %d")
+indices <- read_csv(file="https://data.mbari.org/products/satellite-derived/global-modes/climate_indices.csv", comment = "%")
+indices$time <- as.Date(paste(indices$year, indices$month, rep(15, length(indices$year))), format = "%Y %m %d")
+indices$None <- indices$MEI * 0
 
-# Define UI for application that draws a histogram
+# Define UI for application that draws timeseries
 ui <- fluidPage(
-  
-  # Application title
-  titlePanel("Climate indices visualization"),
-  
-  # Sidebar with indices selection
+  titlePanel(title = "Climate indices visualization"),
   sidebarLayout(
-    sidebarPanel(
-      selectInput("indexname1",label="Index 1 (shaded)",choices=c("M1","M2","M3","M4","M5","M6")),
-      selectInput("indexname2",label="Index 2 (line)",choices=c("MEI","PDO","NPGO","EMI","Nino3","Nino4","AMO","NAO","ATL3")),
-      width=2
+    sidebarPanel = sidebarPanel(
+      selectInput(
+        inputId = "indexname1",
+        label = "Index 1 (shaded)",
+        choices = c("M1", "M2", "M3", "M4", "M5", "M6")
+      ),
+      selectInput(
+        inputId = "indexname2",
+        label = "Index 2 (line)",
+        choices = c(
+          "None",
+          "MEI",
+          "PDO",
+          "NPGO",
+          "EMI",
+          "Nino3",
+          "Nino4",
+          "AMO",
+          "NAO",
+          "ATL3"
+        )
+      ),
+      width = 2
     ),
-    
-    # Show a plot comparing climate indices
-    mainPanel(
-      plotlyOutput("distPlot")
-    )
+    mainPanel = mainPanel(plotlyOutput(outputId = "distPlot"))
   )
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required to draw plots
 server <- function(input, output) {
-  
-  output$distPlot <- renderPlotly({
+
+    output$distPlot <- renderPlotly({
 
     # max & min will be used to scale the 2nd plot
-    max1 <- max(Mset[,input$indexname1],na.rm=TRUE)
-    max2 <- max(indices[,input$indexname2],na.rm=TRUE)
-    
+    max1 <- max(Mset[, input$indexname1], na.rm = TRUE)
+    max2 <- max(indices[, input$indexname2], na.rm = TRUE)
+
     # Mset$varpos & $varneg will be used to display anomalies
-    Mset[,"varpos"] <- Mset[,input$indexname1]
-    Mset[,"varneg"] <- Mset[,input$indexname1]
-    Mset$varpos[which(Mset$varpos > 0)] <- rep(0, length(which(Mset$varpos > 0)))
-    Mset$varneg[which(Mset$varneg < 0)] <- rep(0, length(which(Mset$varneg < 0)))
+    Mset[, "varneg"] <- Mset[, input$indexname1]
+    Mset[, "varpos"] <- Mset[, input$indexname1]
+    Mset$varneg[which(Mset$varneg > 0)] <- rep(0, length(which(Mset$varneg > 0)))
+    Mset$varpos[which(Mset$varpos < 0)] <- rep(0, length(which(Mset$varpos < 0)))
+
     
-    # compute correlation that will be displayed in the legend
+    if (str_equal(input$indexname2, "None")) {
+
+	# define simple plot (no index)
+    p <- ggplot() +
+      geom_area(data = Mset, aes(x = time, y = varneg), fill = "blue") +
+      geom_area(data = Mset, aes(x = time, y = varpos), fill = "red") +
+      scale_x_date(limits = c(min(Mset$time)-15, max(Mset$time)+15), expand=c(0, 0)) +
+      labs(x = "Time", y = input$indexname1)
+
+    } else {
+
+	# compute correlation that will be displayed in the legend	
     commonxM <- Mset$time %in% indices$time
     commonxI <- indices$time %in% Mset$time
-    subsetM <- Mset[commonxM,input$indexname1]
-    subsetI <- indices[commonxI,input$indexname2]
+    subsetM <- Mset[commonxM, input$indexname1]
+    subsetI <- indices[commonxI, input$indexname2]
     iok1 <- !is.na(subsetM) & !is.na(subsetI)
-    r=cor(subsetM[iok1,],subsetI[iok1,])
-    legname <- paste(input$indexname2," (r = ",toString(floor(r*100)/100),")",sep="")
-    
+    r <- cor(subsetM[iok1, ], subsetI[iok1, ])
+    legname <- paste(input$indexname2, " (r = ", toString(floor(r*100)/100), ")", sep = "")
+
     # define plot
     p <- ggplot() +
-      geom_area(data = Mset, aes(x=time, y=varpos), fill="blue") +
-      geom_area(data = Mset, aes(x=time, y=varneg), fill="red") +
-      geom_line(data = indices, aes(x=time, y=eval(as.name(input$indexname2))/(max2/max1), color=legname),size=0.5) +
-      scale_y_continuous(sec.axis = sec_axis(trans = ~.*(max2/max1),name = input$indexname2)) +
-      scale_x_date(limits = c(min(Mset$time),max(Mset$time)),expand=c(0,0)) +
-      labs(x  ="Time", y = input$indexname1) +
-      theme(legend.key.width=unit(3,"line"),legend.background = element_rect(fill="transparent"),legend.text=element_text(size=11)) +
-      scale_colour_manual("", 
+      geom_area(data = Mset, aes(x = time, y = varneg), fill = "blue") +
+      geom_area(data = Mset, aes(x = time, y = varpos), fill = "red") +
+      geom_line(data = indices, aes(x = time, y = eval(as.name(input$indexname2))/(max2/max1), color = legname), linewidth = 0.5) +
+      scale_y_continuous(sec.axis = sec_axis(trans = ~.*(max2/max1), name = input$indexname2)) +
+      scale_x_date(limits = c(min(Mset$time)-15, max(Mset$time)+15), expand=c(0, 0)) +
+      labs(x = "Time", y = input$indexname1) +
+      theme(legend.key.width=unit(3,"line"), legend.background = element_rect(fill="transparent"), legend.text = element_text(size = 11)) +
+      scale_colour_manual("",
                           breaks = c(legname),
                           values = c("black"))
-    
+    }
+
     # display plot & fix legend location issue (ggplotly would remove the legend position)
-    p2 <- plotly_build(p)   
-    style(p2) %>% layout( legend = list(x = 0.01, y = 1) )  
-      
+    p2 <- plotly_build(p)
+    # style(p2) %>% layout(legend = list(x = 0.01, y = 1)) 		# now causes the app to crash.
+
   })
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
